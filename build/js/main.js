214a701e75edca0dd9d530c6ee31cf9b16eec5d3
Евгения Борисова <borisova.ev.2019@yandex.ru>
2021-09-19 08:37:08 +0300

    Добавляет шапку проекта

--- build/js/main.js
+++ build/js/main.js
@@ -7,15 +7,12 @@ const onMenuButtonClick = () => menu.classList.toggle('main-navigation--closed')
 
 function openCloseMenu() {
   menu.classList.remove('main-navigation--nojs');
-  console.log(1);
   menuButton.addEventListener('click', function () {
     menu.classList.toggle('main-navigation--closed');
     menu.classList.toggle('main-navigation--opened');
     const attribute = menu.classList.contains('main-navigation--opened') ? 'Закрыть меню' : 'Открыть меню';
     menuButton.setAttribute('aria-label', attribute);
-    console.log(5);
   });
-  console.log(2);
 }
 
 openCloseMenu();
